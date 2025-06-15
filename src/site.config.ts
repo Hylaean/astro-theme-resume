270ed9ef4dc5721fc48af27895c96930b621ce26
Jean Llorca <jean@hylaean.com>
2025-06-15 13:42:59 +0200

    Advance resume

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-theme-resume',
-	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+       author: 'Jean Llorca',
+       // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+       title: 'Jean Llorca Resume',
+       // Meta property used as the default description meta property
+       description: 'The professional profile of Jean Llorca',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
